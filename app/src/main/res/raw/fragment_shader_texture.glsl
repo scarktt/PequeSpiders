6724a06a486c5fa89176ad1fd616775d89ca496d
soteloserrano <ssoteloserrano@gmail.com>
2018-06-16 18:30:43 -0600

    Se modifica el fragment shader

--- app/src/main/res/raw/fragment_shader_texture.glsl
+++ app/src/main/res/raw/fragment_shader_texture.glsl
@@ -1,7 +1,15 @@
 precision mediump float;
 varying vec2 v_texCoord;
+uniform sampler2D b_texture;
 uniform sampler2D s_texture;
+vec4 col1;
+vec4 col2;
 
 void main() {
-    gl_FragColor = texture2D( s_texture, v_texCoord );
+    /*col1 = texture2D(b_texture, v_texCoord );
+    col2 = texture2D(s_texture, v_texCoord );
+    //gl_FragColor = col1 + col2;
+    gl_FragColor = col1 * col2;
+    gl_FragColor = vec4(col1.rgb * (1.0f - col2.a) + col2.rgb * col2.a, 1.0f);*/
+    gl_FragColor = texture2D(b_texture, v_texCoord);
 }
