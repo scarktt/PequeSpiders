edc95b1f1295a2af59c6f7cfe65783abd63bec89
soteloserrano <ssoteloserrano@gmail.com>
2018-06-16 18:27:18 -0600

    Se agrega el background

--- app/src/main/java/com/example/pequespiders/GLSurfaceview.java
+++ app/src/main/java/com/example/pequespiders/GLSurfaceview.java
@@ -19,6 +19,10 @@ public class GLSurfaceview extends GLSurfaceView {
 
         // Renderiza la ventana solo cuando hay un cambio en los "dibujos"
         setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+
+        this.setBackgroundResource(R.drawable.background);
+        this.setZOrderOnTop(true);
+
     }
 
     @Override
